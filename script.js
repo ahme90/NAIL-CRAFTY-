e94350a924a56b21bf068cb244e8ce255e979bde
ahmedgjg <ahme90569@gmail.com>
2024-04-29 18:55:01 +0500

    as

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
+timeline1 = document.querySelector("#btn1");
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
@@ -8,6 +9,9 @@ burger.addEventListener("click", () => {
 const kids_sec= document.querySelector(".button-learn");
 const adult_sec= document.querySelector(".button-learn2");
 const contact = document.querySelector("#cnt")
+timeline1.addEventListener("click", () => {
+  window.location.href = "glitteraura.html";
+  });
 cnt.addEventListener("click", () => {
   window.location.href = "contact_page.html";
   });
