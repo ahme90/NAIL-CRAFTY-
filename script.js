42012ff0a04035c32b71fcc6be57761c95be1943
ahmedgjg <ahme90569@gmail.com>
2024-04-30 16:03:43 +0500

    links done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 timeline1 = document.querySelector("#btn1");
+timeline2 = document.querySelector("#btn2");
+timeline3 = document.querySelector("#btn3");
+timeline4 = document.querySelector("#btn4");
+timeline5 = document.querySelector("#btn5");
 const sign = document.querySelector(".btn-12");
 
 burger.addEventListener("click", () => {
@@ -10,9 +14,21 @@ burger.addEventListener("click", () => {
 const kids_sec= document.querySelector(".button-learn");
 const adult_sec= document.querySelector(".button-learn2");
 const contact = document.querySelector("#cnt")
-timeline1.addEventListener("click", () => {
-  window.location.href = "glitteraura.html";
+timeline5.addEventListener("click", () => {
+  window.location.href = "jewels.html";
   });
+timeline4.addEventListener("click", () => {
+  window.location.href = "french.html";
+  });
+timeline3.addEventListener("click", () => {
+  window.location.href = "jelly_nails.html";
+  });
+  timeline1.addEventListener("click", () => {
+    window.location.href = "glitteraura.html";
+    });
+  timeline2.addEventListener("click", () => {
+    window.location.href = "cateye.html";
+    });
   sign.addEventListener("click", () => {
     window.location.href = "signup-in.html";
     });
